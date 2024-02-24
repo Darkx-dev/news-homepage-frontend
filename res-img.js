2b43e0280e236a2d8e970dd8877acf5024c0d788
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-02-24 05:53:31 +0530

    main

--- res-img.js
+++ res-img.js
@@ -1,3 +1,3 @@
 const imgElement = document.querySelector(".main-image-container img")
-if (window.innerWidth > 1024) imgElement.src = "/assets/images/image-web-3-desktop.jpg"
-else imgElement.src = "/assets/images/image-web-3-mobile.jpg"
+if (window.innerWidth > 1024) imgElement.src = "assets/images/image-web-3-desktop.jpg"
+else imgElement.src = "assets/images/image-web-3-mobile.jpg"
