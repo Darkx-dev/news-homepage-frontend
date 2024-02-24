f96be5dea7520757a4f2c0783a263180a46e1f59
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-02-24 07:03:07 +0530

    main

--- res-img.js
+++ res-img.js
@@ -1,3 +1,17 @@
 const imgElement = document.querySelector(".main-image-container img")
+const dropElement = document.querySelector(".navs-container")
+const toggleBtnImg = document.querySelector("button.sidebar-drop img")
+let toggleCheck = false
+
 if (window.innerWidth > 1024) imgElement.src = "assets/images/image-web-3-desktop.jpg"
 else imgElement.src = "assets/images/image-web-3-mobile.jpg"
+
+const handleDrop = () => {
+    if (toggleCheck == false) {
+        dropElement.style.transform = "translateX(0px)"
+        toggleCheck = true
+    } else {
+        dropElement.style.transform = "translateX(100%)"
+        toggleCheck = false
+    }
+}
